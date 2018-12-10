02bdb5198bb562e6977c77dd71e169a13671ffb8
fractalfeat <fractalfeat@gmail.com>
2018-12-09 20:46:29 -0800

    js not working yet

--- js/app.js
+++ js/app.js
@@ -1,45 +1,46 @@
 'use strict';
 
-alert('Happy Birthday Jasmine!!!');
-
-// Question 1
-
-var gamePlayRes = prompt('Do you want to play a game?');
-gamePlayRes = gamePlayRes.toLowerCase();
-function gamePlay () {
-  if (gamePlayRes === null || gamePlayRes === 'n' || gamePlayRes === 'no') {
-    alert('Sorry. You have to play any ways...');
-  } else {
-    alert('Yay!! Let\'s play!!');
-  }
+var gamePlayRes = confirm('Do you want to play a game?');
+if(gamePlayRes) {
+  alert('Yay! Let\'s play!');
+} else {
+  alert('Sorry. You have to play any ways...');
 }
-gamePlay();
+console.log(gamePlayRes);
 
+var nounNameRes = prompt('Name a noun, please.');
+if(nounNameRe === null); {
+  alert('Please give me something to work with, thank you.');
+} else {
+  alert('Thank you!')
+}
+console.log(nounNameRes);
 
-// Question 2
+var numPick = prompt('Pick a number.');
+if(numPick === null) {
+  alert('Just pick something, please.');
+} else {
+  alert('Thank you!');
+}
+console.log(numPick);
 
-
-var nounName = prompt('Name a noun.');
-nounName = nounName.toLowerCase();
-function nounNameRes () {
-    if (nounName === null || nounName === no || nounName === NO || nounName === N || nounName === n);
-    alert('Please give me something to work with?'); 
-  } else if {
-    alert('Wonderful choice!');localStorage
-  };
-
-nounNameRes();
-
-
-// Question 3
-
-var adjectiveName = prompt('Name an adjective.');
+var adjectiveName = prompt('Name an adjective, please.');
 adjectiveName = adjectiveName.toLowerCase();
 function adjectiveNameRes () {
     if (nounName === null || nounName === no || nounName === NO || nounName === N || nounName === n);
-    return'You have to put something in.';
+    return('You have to put something in.');
   } else {
-    return'Thank you!';
+    return('Thank you!');
 };
+console.log(adjectiveNameRes();
 
-console.log(adjectiveName());
+var verbName = prompt('Name a verb, please.');
+verbName = verbName.toUpperCase();
+function verbNameRes () {
+  if (verbName === n || verbName === no || verbName === null);
+  return('Just pick a verb...');
+} else {
+  return('Thanks!');
+};
+console.log(verbName)
+
